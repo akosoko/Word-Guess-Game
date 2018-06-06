267e6f06b3b75c717421999f0498008bb55b0d05
ayanna kosoko <ayannakosoko@gmail.com>
2018-06-05 21:19:15 -0400

    updated

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -2,9 +2,10 @@
 $(document).ready(function() {
 
     var wins = 0;
-    var numberOfGuesses = 9;
+    var validGuesses = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
 
     function Game() {
+
         this.rappers = [
             "kanye",
             "drake",
@@ -14,18 +15,22 @@ $(document).ready(function() {
             "faboulous",
             "meek"
         ]
-    
+
         this.word = this.rappers[Math.floor(Math.random() * this.rappers.length)];
         this.guessedLetters = [];
         this.errors = 0;
         this.visibleLetters = [];
         this.gameOver = false;
+        this.numberOfGuesses = 9;
         for (var i = 0; i < this.word.length; i++) {
             this.visibleLetters[i] = (false);
         }
     }
     var game = new Game();
 
+    console.log(game.word)
+
+    //set empty song title
     var tempString = "";
 	for (var i = 0; i < game.visibleLetters.length; i++) {
 		tempString += ((game.visibleLetters[i] || game.gameOver) ? game.word.charAt(i).toUpperCase() : "_");
@@ -34,55 +39,136 @@ $(document).ready(function() {
     
     $("#song-title").text(tempString);
     $("#guessed-letters").text(game.guessedLetters);
-    $("#number-of-guesses-remaining").text(numberOfGuesses);
+    $("#number-of-guesses-remaining").text(game.numberOfGuesses);
     $("#number-of-wins").text(wins);
 
-    $(document).keypress(function(event){
-	
-        console.log(event.key)
-        game.guessedLetters.push(event.key);
-        $("#guessed-letters").text(game.guessedLetters);
+    document.onkeypress = function(event) {
 
-        var isInWord = false;
-        for (var i = 0; i < game.word.length; i++) {
-            if (game.word.charAt(i) === event.key) {
-                isInWord = true;
-                game.visibleLetters[i] = true;
+        var userGuess = " "
+        if (checkValidKey(event.key)) {
+            userGuess = event.key
+            
+            if(!(returnSongTitle(userGuess) == "")) {
+            $("#song-title").text(returnSongTitle(userGuess))
+            } 
+
+            var alreadyGuessed = false;
+            for( var z = 0; z < game.guessedLetters.length; z++) {
+                if ((game.guessedLetters[z] == userGuess)) {
+                    alreadyGuessed = true;
+                }
             }
-        }
-        if (!isInWord) {
-            game.errors++;
-            if (numberOfGuesses > 0) {}
-                numberOfGuesses -= game.errors;
+            if (!(wordContainsLetter(userGuess)) && (!alreadyGuessed)) {
+                game.guessedLetters.push(userGuess)
             }
-            $("#number-of-guesses-remaining").text(numberOfGuesses);
-        }
+            
+            $("#guessed-letters").text(game.guessedLetters);
 
-        if (game.errors >= numberOfGuesses) {
-            game.gameOver = true;
-        }
+            $("#number-of-guesses-remaining").text(returnNumberOfRemainingGuesses(game.guessedLetters.length, game.numberOfGuesses, userGuess))
 
-        if (!game.visibleLetters.includes(false)) {
-            wins++;
-            game.gameOver = true;
+            if (userWon(returnSongTitle(userGuess)) || (returnNumberOfRemainingGuesses(game.guessedLetters.length, game.numberOfGuesses, userGuess) == 0)) {
+                $("#number-of-wins").text(wins)
+                if ((returnNumberOfRemainingGuesses(game.guessedLetters.length, game.numberOfGuesses, userGuess) == 0)) {
+                    if (confirm("You Lose!")) {
+                        reset()
+                    }
+                } else {
+                    wins++;
+                    $("#song-title").text(returnSongTitle(userGuess))
+                    if (confirm("You Win!")) {
+                        reset()
+                    }
+                }
+            }
+    }
+    };
+
+    function returnGuessedLetter(key) {
+        return key
+    }
+
+    function returnNumberOfWins(wins){
+        return wins
+    }
+
+    function reset() {
+        game = new Game();
+
+        console.log(game.word)
+
+        //set empty song title
+        var tempString = "";
+        for (var i = 0; i < game.visibleLetters.length; i++) {
+            tempString += ((game.visibleLetters[i] || game.gameOver) ? game.word.charAt(i).toUpperCase() : "_");
+            if (i < (game.visibleLetters.length - 1)) tempString += " ";
         }
         
-    });
+        $("#song-title").text(tempString);
+        $("#guessed-letters").text(game.guessedLetters);
+        $("#number-of-guesses-remaining").text(game.numberOfGuesses);
+        $("#number-of-wins").text(wins);
+    }
 
+    function returnNumberOfRemainingGuesses(guessedLettersLength, numberOfGuesses, userGuess) {
 
+        for (var i = 0; i < game.word.length; i++) {
+            if (!(game.word.charAt(i) == userGuess) && (numberOfGuesses > 0)) {
+                numberOfGuesses = 9
+                numberOfGuesses = numberOfGuesses - guessedLettersLength
+            }
+        }
 
-    // tempString = "";
-	// for (var i = 0; i < this.guessedLetters.length; i++) {
-	// 	tempString += (this.guessedLetters[i].toUpperCase());
-	// 	if (i < (this.guessedLetters.length - 1)) tempString += " ";
-	// }
-	// for (var i = tempString.length; i < 9; i++) {
-	// 	tempString += " ";
-    // }
-    // $("#number-of-guesses-remaining").text(tempString);
-	
+        return numberOfGuesses
+    }
 
+    function wordContainsLetter(userGuess) {
+        var contains = false;
+        for (var i = 0; i < game.word.length; i++) {
+            if (game.word.charAt(i) == userGuess) {
+                contains = true;
+            }
+        }
+        return contains
+    }
 
-    console.log(game.word);
+    function checkValidKey(userGuess) {
+        var valid = false;
+        for (var i = 0; i < validGuesses.length; i++) {
+            if ((validGuesses[i] == userGuess)) {
+                valid = true;
+            }
+        }
+        return valid
+    }
 
+    function userWon(songtitle) {
+        var blanks = 0;
+        var won = false;
+        for (var i = 0; i < songtitle.length; i++) {
+            if ((songtitle.charAt(i) == "_")) {
+                blanks++;
+            }
+        }
+        console.log(blanks);
+        if (blanks == 0) {
+            won = true;
+        }
+        return won
+    }
+
+    function returnSongTitle(userGuess) {
+        var newString  = "";
+        for (var i = 0; i < game.word.length; i++) {
+            if (game.word.charAt(i) == userGuess) {
+                game.visibleLetters[i] = true
+            }
+        } 
+        
+        for (var i = 0; i < game.visibleLetters.length; i++) {
+            newString += ((game.visibleLetters[i] || game.gameOver) ? game.word.charAt(i).toUpperCase() : "_");
+            if (i < (game.visibleLetters.length - 1)) newString += " ";
+        }
+
+        return newString
+    }   
 });
